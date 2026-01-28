9c82a1d657f4b40f6b6bab3bb5f833a2b30a6f80
Andres Gomez <13159467+agomez8@users.noreply.github.com>
2026-01-27 21:59:45 -0800

    Add files via upload

---
+++ src/data_handler/Table2Parser.java
@@ -0,0 +1,126 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data_handler;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+
+/**
+ *
+ * @author user
+ */
+public class Table2Parser {
+    
+    /**
+     * Reads raw output file and outputs Table 1 from the paper. <br>
+     * @param args: path to raw output file, path to table file 
+     */
+    public static void main(String[] args) throws IOException {
+        writeSolution(args[1], readSolution(args[0]));
+    }
+    
+    
+    
+     /**
+     * Reads the data from file. <br>
+     *
+     * @param path Path to file
+     */
+    public static Map<String,double[]> readSolution(String path) {
+       
+        BufferedReader br = null;
+        String cvsSplitBy = ",";
+        String line;
+        String[] row, value;
+        Map<String,double[]> table= new HashMap<>();
+        
+
+        try {
+            br = new BufferedReader(new FileReader(path));
+            while ((line = br.readLine()) != null) {
+                row = line.split(cvsSplitBy);
+                int tau=Integer.parseInt(row[5].trim());
+                int k= Integer.parseInt(row[4].trim());
+                double lambda=Double.parseDouble(row[3].trim());
+                int n = Integer.parseInt(row[1].trim());
+                String name = tau+" "+k+" "+lambda+" "+n;
+                double[] values=table.get(name);
+                if(values==null)
+                    values = new double[13];
+                if(Integer.parseInt(row[7].trim())==0) // Mosek
+                {
+                    values[0]+=1; // Number of instances solved with mosek observed with this key
+                    values[1]+=Double.parseDouble(row[12].trim()); // Time used by Mosek
+                    values[2]+=Double.parseDouble(row[17].trim()); // Nodes explored by Mosek
+                    values[3]+=Double.parseDouble(row[18].trim())<1e-4?1:0; // Number of instances solved to optimality
+                }
+                else // DD
+                {
+                    values[4]+=1; // Number of instances solved with DD observed with this key
+                    values[5]+=Double.parseDouble(row[9].trim()); // Number of arcs in the DD
+                    values[6]+=Double.parseDouble(row[10].trim()); // Time to construct the DD
+                    values[7]+=Double.parseDouble(row[11].trim()); // Time to solve the shortest path in the DD
+                    values[8]+=values[6]<600?1:0; // Number of instances solved to to optimality
+                    values[9]+=Double.parseDouble(row[12].trim()); // Time to solve the SOCP relaxation
+                    values[10]+=Double.parseDouble(row[22].trim()); // Condition number
+                    values[11]+=Double.parseDouble(row[20].trim()); // Decisions to remember
+                    values[12]+=Double.parseDouble(row[21].trim()); // Nodes per layer
+                }
+                table.put(name, values);
+            }
+
+        } catch (FileNotFoundException e) {
+        } catch (IOException e) {
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+        return table;
+    }
+    
+    /**
+     * Writes the table to a CVS file. <br>
+     *
+     * @param path Path to file
+     * @param table Table with the information
+     */
+    public static void writeSolution(String path, Map<String,double[]> table) throws IOException {
+       
+        try (FileWriter out = new FileWriter(new File(path), false)) {
+//            out.write(F.length+","+F[0].length+"\n");
+            out.write("tau k lambda n, cond,truncated_m,truncated_layer, time_msk, nodes_msk, perc_msk, arcs_dd, time_dd, time_sp, perc_dd, time_socp\n");
+            for (Map.Entry<String, double[]> entry : table.entrySet()) {
+                
+                String key = entry.getKey();
+                double[] val = entry.getValue();
+                out.write(key+","+val[10]/val[4]+","+val[11]/val[4]+","+val[12]/val[4]+",");
+                for (int i = 1; i <= 3; i++) {
+                    out.write(val[i]/val[0]+",");
+                }
+                for (int i = 5; i <= 9; i++) {
+                    out.write(val[i]/val[4]+",");
+                }
+                out.write("\n");
+                
+            }
+
+        }
+    }
+    
+}
